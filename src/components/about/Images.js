6db0fd749e2d55d699e6d945ed6b8b8a5953d708
Daniel Márquez <daniel.marquez@ciclosmontecastelo.com>
2023-07-14 19:17:21 +0200

    about done

---
+++ src/components/about/Images.js
@@ -0,0 +1,17 @@
+function Images() {
+  return (
+    <div className="w-full">
+      <div className="grid grid-cols-2 gap-8">
+        <img
+          src="https://img.freepik.com/free-photo/architect-with-wireless-headset-using-laptop-while-working-home-night-sitting-kitchen-industrial-female-engineer-studying-personal-computer-showing-cad-software_482257-14609.jpg?w=1380&t=st=1689352662~exp=1689353262~hmac=de66e1ee642351ee933c1a38faf49bd3a75f9b4dd264786722724d7b888cf79a"
+          className="w-full h-96 object-cover"
+        />
+        <img
+          src="https://img.freepik.com/free-psd/laptop-phone-marble-table_53876-57703.jpg?w=1060&t=st=1689352369~exp=1689352969~hmac=663fd77f102115cd91fd663fb162e25bba0de5d26a22531cb40872bc60fc22bb"
+          className="w-full h-96 object-cover"
+        />
+      </div>
+    </div>
+  );
+}
+export default Images;
