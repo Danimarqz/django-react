432f2f871fd6a218b7c3040f751d5e52bafeffd3
Daniel Márquez <daniel.marquez@ciclosmontecastelo.com>
2023-07-15 18:30:41 +0200

    categories serializer

--- apps/category/views.py
+++ apps/category/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import permissions
+from .models import Category
 
-# Create your views here.
+
+class ListCategoriesView(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def get(self, request, format=None):
+        if Category.objects.all().exists:
+            return
+        else:
+            return Response({"message": "No categories found"}, status=status.HTTP_404_NOT_FOUND)
