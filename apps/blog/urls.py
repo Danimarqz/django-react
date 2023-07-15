77dcd730eec29600d7d3f964b8733a4c5692d345
Daniel Márquez <daniel.marquez@ciclosmontecastelo.com>
2023-07-15 19:10:59 +0200

    blogs views models and serializer

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import *
 
 ulrpatterns = [
-    path('list', )
+    path('list', BlogListView.as_view())
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import permissions
+from .models import Post
+from apps.category.models import Category
+from .serializers import PostSerializer
 
-# Create your views here.
+class BlogListView(APIView):
+    permission_classes = (permissions.AllowAny,)
+    def get(self, request, format=None):
+        if Post.objects.all().exists():
+                return Response({'posts': ''}, status=status.HTTP_200_OK)
+        else:
+                return Response({'Not found'}, status=status.HTTP_404_NOT_FOUND)
+             
