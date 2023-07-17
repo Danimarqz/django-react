be22967a068846158eb5d24ffc34fcd5faf40519
Daniel Márquez <daniel.marquez@ciclosmontecastelo.com>
2023-07-17 23:00:10 +0200

    blog front started

---
+++ src/components/blog/CategoriesHeader.js
@@ -0,0 +1,7 @@
+export default function CategoriesHeader(categories){
+    return(
+        <div className="w-full bg-gray-100 py-8">
+            CategoriesHeader
+        </div>
+    )
+}
--- src/containers/pages/Blog.jsx
+++ src/containers/pages/Blog.jsx
@@ -6,6 +6,7 @@ import { Helmet } from "react-helmet-async";
 import { get_categories } from "redux/actions/categories/categories";
 import { connect } from "react-redux";
 import { get_blog_list, get_blog_list_page } from "redux/actions/blog/blog";
+import CategoriesHeader from "components/blog/CategoriesHeader";
 
 function Blog({
     get_categories, 
@@ -44,8 +45,8 @@ function Blog({
                 <meta name="twitter:site" content="@dani_marqz_33" />
             </Helmet>
             <Navbar />
-            <div className="pt-28">
-                Blog
+            <div className="pt-24">
+                <CategoriesHeader categories={categories && categories} />
             </div>
             <Footer />
         </Layout>
