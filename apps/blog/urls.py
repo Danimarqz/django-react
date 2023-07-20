377295fbc50ae700f41ce049d9ab9a323a011d86
Daniel Márquez <daniel.marquez@ciclosmontecastelo.com>
2023-07-21 00:32:37 +0200

    fixed backend not paginating, todo postfiltering

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('by_category',ListPostsByCategoryView.as_view()),
     path('detail/<slug>',PostDetailView.as_view()),
     path('search',SearchBlogView.as_view()),
+    
 ]
