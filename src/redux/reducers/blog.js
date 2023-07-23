f08a2bcddde91e242b90cc10f1638814f685217a
Daniel Márquez <daniel.marquez@ciclosmontecastelo.com>
2023-07-23 18:16:28 +0200

    backend works perfectly, need to keep working with redux

--- src/redux/reducers/blog.js
+++ src/redux/reducers/blog.js
@@ -1,13 +1,3 @@
-import {
-  GET_BLOG_LIST_SUCCESS,
-  GET_BLOG_LIST_FAIL,
-  GET_BLOG_LIST_CATEGORIES_SUCCESS,
-  GET_BLOG_LIST_CATEGORIES_FAIL,
-  GET_BLOG_SUCCESS,
-  GET_BLOG_FAIL,
-  GET_SEARCH_BLOG_SUCCESS,
-  GET_SEARCH_BLOG_FAIL,
-} from "../actions/blog/types";
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
