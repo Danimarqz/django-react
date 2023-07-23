2138330c24b12803fbc8cfd740ea198961196ef2
Daniel Márquez <daniel.marquez@ciclosmontecastelo.com>
2023-07-23 18:01:42 +0200

    blog reducer updated

--- src/store.js
+++ src/store.js
@@ -1,17 +1,15 @@
-import { createStore,applyMiddleware } from 'redux';
+import { configureStore } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './redux/reducers'
-import { composeWithDevTools } from 'redux-devtools-extension';
 
 const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(
+const store = configureStore(
     rootReducer,
     initialState,
-    // applyMiddleware(...middleware)
-    composeWithDevTools(applyMiddleware(...middleware))
+    
 );
 
 export default store;
