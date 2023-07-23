6b5484d180bf207cc07b83811f7eb17262876afb
Daniel Márquez <daniel.marquez@ciclosmontecastelo.com>
2023-07-23 18:01:53 +0200

    blog reducer updated

--- src/redux/reducers/blog.js
+++ src/redux/reducers/blog.js
@@ -1,87 +1,80 @@
 import {
-    GET_BLOG_LIST_SUCCESS,
-    GET_BLOG_LIST_FAIL,
-    GET_BLOG_LIST_CATEGORIES_SUCCESS,
-    GET_BLOG_LIST_CATEGORIES_FAIL,
-    GET_BLOG_SUCCESS,
-    GET_BLOG_FAIL,
-    GET_SEARCH_BLOG_SUCCESS,
-    GET_SEARCH_BLOG_FAIL,
-} from '../actions/blog/types';
+  GET_BLOG_LIST_SUCCESS,
+  GET_BLOG_LIST_FAIL,
+  GET_BLOG_LIST_CATEGORIES_SUCCESS,
+  GET_BLOG_LIST_CATEGORIES_FAIL,
+  GET_BLOG_SUCCESS,
+  GET_BLOG_FAIL,
+  GET_SEARCH_BLOG_SUCCESS,
+  GET_SEARCH_BLOG_FAIL,
+} from "../actions/blog/types";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    blog_list: null,
-    blog_list_category: null,
-    filtered_posts: null,
-    post: null,
-    count: null,
-    next: null,
-    previous: null,
+  blog_list: null,
+  blog_list_category: null,
+  filtered_posts: null,
+  post: null,
+  count: null,
+  next: null,
+  previous: null,
 };
-
-export default function blog(state = initialState, action) {
-    const { type, payload } = action;
-
-    switch(type) {
-        case GET_BLOG_LIST_SUCCESS:
-            return {
-                ...state,
-                blog_list: payload.results,
-                count: payload.count,
-                next: payload.next,
-                previous: payload.previous,
-            }
-        case GET_BLOG_LIST_FAIL:
-            return {
-                ...state,
-                blog_list: null,
-                count: null,
-                next: null,
-                previous: null,
-            }
-        case GET_BLOG_SUCCESS:
-            return {
-                ...state,
-                post: payload.post,
-            }
-        case GET_BLOG_FAIL:
-            return {
-                ...state,
-                post: null,
-            }
-        case GET_BLOG_LIST_CATEGORIES_SUCCESS:
-            return {
-                ...state,
-                blog_list_category: payload.results.posts,
-                count: payload.count,
-                next: payload.next,
-                previous: payload.previous,
-            }
-        case GET_BLOG_LIST_CATEGORIES_FAIL:
-            return {
-                ...state,
-                blog_list_category: null,
-                count: null,
-                next: null,
-                previous: null,
-            }
-        case GET_SEARCH_BLOG_SUCCESS:
-            return {
-                ...state,
-                filtered_posts: payload.results.filtered_posts,
-                count: payload.count,
-                next: payload.next,
-                previous: payload.previous,
-            }
-        case GET_SEARCH_BLOG_FAIL:
-            return {
-                ...state,
-                filtered_posts: null,
-                count: null,
-                next: null,
-                previous: null,
-            }
-        default: 
-            return state;
-    }
-}
+const blogSlice = createSlice({
+  name: "blog",
+  initialState: initialState,
+  reducers: {
+    GET_BLOG_LIST_SUCCESS(state, action) {
+      state.blog_list = action.payload.results;
+      state.count = action.payload.count;
+      state.next = action.payload.next;
+      state.previous = action.payload.previous;
+    },
+    GET_BLOG_LIST_FAIL(state, action) {
+      state.blog_list = null;
+      state.count = null;
+      state.next = null;
+      state.previous = null;
+    },
+    GET_BLOG_SUCCESS(state, action) {
+      state.blog_list = action.payload.post;
+    },
+    GET_BLOG_FAIL(state, action) {
+      state.blog_list = null;
+    },
+    GET_BLOG_LIST_CATEGORIES_SUCCESS(state, action) {
+      state.blog_list_category = action.payload.results.posts;
+      state.count = action.payload.count;
+      state.next = action.payload.next;
+      state.previous = action.payload.previous;
+    },
+    GET_BLOG_LIST_CATEGORIES_FAIL(state, action) {
+      state.blog_list_category = null;
+      state.count = null;
+      state.next = null;
+      state.previous = null;
+    },
+    GET_SEARCH_BLOG_SUCCESS(state, action) {
+      state.filtered_posts = action.payload.results.filtered_posts;
+      state.count = action.payload.count;
+      state.next = action.payload.next;
+      state.previous = action.payload.previous;
+    },
+    GET_SEARCH_BLOG_FAIL(state, action) {
+      state.filtered_posts = null;
+      state.count = null;
+      state.next = null;
+      state.previous = null;
+    },
+  },
+});
+export const {
+  GET_BLOG_LIST_SUCCESS,
+  GET_BLOG_LIST_FAIL,
+  GET_BLOG_SUCCESS,
+  GET_BLOG_FAIL,
+  GET_BLOG_LIST_CATEGORIES_SUCCESS,
+  GET_BLOG_LIST_CATEGORIES_FAIL,
+  GET_SEARCH_BLOG_SUCCESS,
+  GET_SEARCH_BLOG_FAIL,
+} = blogSlice.actions;
+export default blogSlice.reducer;
